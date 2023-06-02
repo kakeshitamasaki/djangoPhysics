1e0cec3f98fb63546bc8ee0ec19a0110de22f474
kake <masaki.kakeshita@gmail.com>
2023-06-03 00:45:04 +0900

    5

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Category
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello")
+    #return HttpResponse("hello")
+    category = Category.objects.all()
+    context = {
+        "category":category
+    }
+    return render(request,'myapp/index.html',context)
